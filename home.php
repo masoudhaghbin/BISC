2f9518188d5c2e8c4f360e6e95ea47181802a7e4
masoudhaghbin <masoud.haghbin7@gmail.com>
2014-09-03 15:04:12 +0430

    FirstDay

---
+++ home.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+
+<html>
+
+<head>
+  <title>سایت اشتراک کتاب</title>
+  <link rel="stylesheet" type="text/css" href="home.css">
+</head>
+
+<body>
+
+<div class="firstdiv">
+
+Book Sharing
+<form method="POST" action="">
+<ul id="login">
+	<li id="pagename"></li>
+	<li><input name="user" placeholder="UserName" ></li>
+	<li><input name="pass" placeholder="Password" ></li> 
+</ul>
+</form>
+
+
+</div>
+
+
+</body>
+</html>
