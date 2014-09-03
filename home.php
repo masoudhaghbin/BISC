67ffa6193947a5a1a1a8798d9fef1c416b4ab20a
masoudhaghbin <masoud.haghbin7@gmail.com>
2014-09-03 16:41:07 +0430

    Masoud

--- home.php
+++ home.php
@@ -11,18 +11,37 @@
 
 <div class="firstdiv">
 
-Book Sharing
+<span id="pagename">Book Sharing</span>
 <form method="POST" action="">
-<ul id="login">
-	<li id="pagename"></li>
+<ul id="login" class="signinpart">
+
+	<!--<li><span id="pagename">Login:</span></li>-->
 	<li><input name="user" placeholder="UserName" ></li>
 	<li><input name="pass" placeholder="Password" ></li> 
+	<li><input type="submit" Value="Login" Name="loginBtn"></li>
 </ul>
 </form>
-
-
 </div>
 
-
+<div class = "Categories">
+<ul>
+<li><a href="">Scientific</a></li>
+<li><a href="">Novel</a></li>
+<li><a href="">Philosophy</a></li>
+<li><a href="">Sport</a></li>
+<li><a href="">Kids</a></li>
+<li><a href="">Cooking</a></li>
+<li><a href="">Historic</a></li>
+</ul>
+</div>
+<div class="top">
+<table>
+	<thead>
+		<title>Top 10 Books</title>
+	</thead>
+	<caption>Top 10 Books</caption>
+	<tr>asdasd</tr>
+</table>
+</div>
 </body>
 </html>
